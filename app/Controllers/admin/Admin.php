3579e344fffa4021ccdebfc5302c4d83c6dff586
ushichaturvedi <109645940+ushichaturvedi@users.noreply.github.com>
2024-07-26 18:13:55 +0530

    ayushi2

---
+++ app/Controllers/admin/Admin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers\admin;
+use App\Controllers\BaseController;
+
+
+use App\Models\UserModel;
+class Admin extends BaseController
+{   
+     
+     public $empModel;
+    public function __construct()
+    {
+        if (session()->get('role') != "admin") {
+            echo 'Access denied';
+            exit;
+        }
+        $this->empModel = new UserModel();
+        // $this->brand = new BrandModel();
+        // $this->productModel = new ProductModel();
+        //  $this->serviceModel = new ServiceModel();
+        //  $this->categoryModel = new CategoryModel();
+        //  $this->CommunityModel = new CommunityModel();
+      
+
+    }
+   public function index(){
+      
+    $data['title'] = "Admin | Dashboard";
+    $data['page'] = ADMIN."index";
+    $data['page_name'] = "Dashboard ";
+//     $data['users'] = $this->empModel->builder()->countAll();
+//    $data['primary'] = $this->empModel->builder()->where("role","primary")->countAllResults();
+//     $data['brands'] = $this->brand->builder()->countAll();
+//     $data['product'] =  $this->productModel->builder()->countAll();
+//     $data['service'] =  $this->serviceModel->builder()->countAll();
+    
+    
+    
+   
+    return view($data['page'],$data);
+   }}
